47a63bb8e5e4d3433a7bc86adf26f2f3fc3f024b
Shoegge <joel.niederhauser@axa.ch>
2013-05-13 10:16:09 +0200

    Test

--- src/main/java/ch/lepeit/brain2/Stage.java
+++ src/main/java/ch/lepeit/brain2/Stage.java
@@ -2,6 +2,10 @@ package ch.lepeit.brain2;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
+/**
+ * @author C925845
+ * Wir später ersetzt. 
+ */
 @Deprecated
 @XmlRootElement
 public enum Stage {
