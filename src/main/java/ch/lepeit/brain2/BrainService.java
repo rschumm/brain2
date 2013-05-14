38058c3ea0b973634ad0cd3b795ca3a7f5196ad0
Shoegge <joel.niederhauser@axa.ch>
2013-05-14 15:37:33 +0200

    fix #5

--- src/main/java/ch/lepeit/brain2/BrainService.java
+++ src/main/java/ch/lepeit/brain2/BrainService.java
@@ -3,12 +3,23 @@ package ch.lepeit.brain2;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import ch.lepeit.brain2.model.DeployArtefakt;
 import ch.lepeit.brain2.model.Stage;
 
 public class BrainService {
 
+    @PersistenceContext
+    private EntityManager em;
+    
     public List<Stage> getAllStages() {
         return Arrays.asList(Stage.values()); 
+    }
+    
+    public List<DeployArtefakt> getAllDepArts(){
+        return null;
     }
 
 }
