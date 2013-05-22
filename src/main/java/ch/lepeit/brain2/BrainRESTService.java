da1dbc690ff2cadfd0a978c0f9e52c9aa4aa0547
Shoegge <joel.niederhauser@axa.ch>
2013-05-22 09:28:30 +0200

    API

--- src/main/java/ch/lepeit/brain2/BrainRESTService.java
+++ src/main/java/ch/lepeit/brain2/BrainRESTService.java
@@ -13,56 +13,48 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import ch.lepeit.brain2.model.Installation;
 import ch.lepeit.brain2.model.Stage;
-
 
 @Path("/brain2")
 @RequestScoped
 public class BrainRESTService {
-	
+
     @Inject
     private Logger log;
-    
+
     @Inject
-    private BrainService brainService; 
-    
+    private BrainService brainService;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Stage> listAllStages() {
-    	log.info("liste stages"); 
+        log.info("liste stages");
         return brainService.getAllStages();
     }
-    
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Stage> listStageOne(){
-        return null;
-    }
-    
+
     @GET
     @Path("/{name}")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Stage> getStageForName(@PathParam("name")String name){
-    log.info("forName");
-    return BrainService.getStageForName(name);
+    public List<Stage> getStageForName(@PathParam("name")
+    String name) {
+        log.info("forName");
+        return brainService.getStageForName(name);
     }
-    
-    
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public void addStage(List<Stage> stages){
-    log.info("add");
-    BrainService.addStage(List<Stage>);
+    public void addInstallation(Installation installation){
+        log.info("add");
+        brainService.addInstallation();
     }
-    
-    
+
     @GET
     @Path("/count")
     @Produces(MediaType.APPLICATION_JSON)
-    public int count(){
-    log.info("count");
-    return BrainService.count();
+    public int count() {
+        log.info("count");
+        return brainService.count();
     }
 
-
-    
 }
