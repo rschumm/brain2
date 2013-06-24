a5ad6c8df3a1d43c111267bfed1f1b43e2117a20
Rémy Schumm <remy.schumm@axa.ch>
2013-06-24 17:04:49 +0200

    gebastel

--- src/test/java/ch/lepeit/brain2/arq/RESTTest.java
+++ src/test/java/ch/lepeit/brain2/arq/RESTTest.java
@@ -1,17 +1,24 @@
-package ch.lepeit.brain2.arq; 
+package ch.lepeit.brain2.arq;
 
 import java.net.URL;
 import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response.Status;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
-import org.jboss.resteasy.client.ClientRequest;
 import org.jboss.resteasy.client.ClientResponse;
+import org.jboss.resteasy.util.GenericType;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -21,32 +28,44 @@ import ch.lepeit.brain2.model.Stage;
 @RunWith(Arquillian.class)
 @RunAsClient
 public class RESTTest {
-    //private static final String RESOURCE_PREFIX = JaxRsActivator.class.getAnnotation(ApplicationPath.class).value().substring(1);
+    // private static final String RESOURCE_PREFIX =
+    // JaxRsActivator.class.getAnnotation(ApplicationPath.class).value().substring(1);
 
     @Deployment(testable = false)
     public static WebArchive createDeployment() {
         return ShrinkWrap.create(WebArchive.class, "test.war").addPackage(BrainRESTService.class.getPackage())
-                .addClasses(EntityManagerProducer.class)
-                .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
-                .addAsResource("import.sql")
+                .addPackage(Stage.class.getPackage()).addClasses(EntityManagerProducer.class)
+                .addAsResource("META-INF/persistence.xml").addAsResource("import.sql")
                 .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
     @ArquillianResource
     URL deploymentUrl;
 
+    // @Test
+    // public void testGetStages() throws Exception {
+    // //deploymentUrl = new URL("http://localhost:8180/test/");
+    // // GET http://localhost:8080/test/rest/customer/1
+    // ClientRequest request = new ClientRequest("http://localhost:8080/brain2/rest/brain2/stages");
+    // //request.header("Accept", MediaType.APPLICATION_XML);
+    //
+    // // we're expecting a String back
+    // ClientResponse<List<Stage>> responseObj = request.get();
+    //
+    // System.out.println(responseObj.toString());
+    //
+    //
+    // }
+
     @Test
-    public void testGetStages() throws Exception {
-        //deploymentUrl = new URL("http://localhost:8180/test/");
-        // GET http://localhost:8080/test/rest/customer/1
-        ClientRequest request = new ClientRequest("http://localhost:8080/brain2/rest/brain2/stages");
-        //request.header("Accept", MediaType.APPLICATION_XML);
+    @GET
+    @Path("rest/brain2/stages")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void testWieVieleStages(ClientResponse<List<Stage>> response) {
+        Assert.assertEquals(Status.OK.getStatusCode(), response.getStatus());
 
-        // we're expecting a String back
-        ClientResponse<List<Stage>> responseObj = request.get(); 
-        
-        System.out.println(responseObj.toString());
-
-
+        List<Stage> stages = response.getEntity(new GenericType<List<Stage>>() {
+        });
+        Assert.assertEquals(3, stages.size());
     }
 }
