281c211dc047b3621080c3f355aaf340e3dc47f9
Rémy Schumm <remy.schumm@axa.ch>
2013-07-08 15:51:37 +0200

    controller baustelle

--- src/main/java/ch/lepeit/brain2/Controller.java
+++ src/main/java/ch/lepeit/brain2/Controller.java
@@ -1,17 +1,23 @@
 package ch.lepeit.brain2;
 
+import java.util.List;
 import java.util.logging.Logger;
 
+import javax.enterprise.context.ConversationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import ch.lepeit.brain2.model.Stage;
 
 import ch.lepeit.brain2.model.Version;
 
 
 @Named
-//scope.... 
+@ConversationScoped 
 public class Controller {
 
+    Version version; 
+    
     @Inject
     private Logger log;
 
@@ -27,4 +33,8 @@ public class Controller {
         
     }
     
+    
+    public List<Stage> getAllStages(){
+        return brainService.getAllStages(); 
+    }
 }
