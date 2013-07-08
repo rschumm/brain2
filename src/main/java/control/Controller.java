43328a37ea1f2e780eb2e9e96a227aa49d780f5e
Shoegge <joel.niederhauser@axa.ch>
2013-07-08 15:41:44 +0200

    jsf

--- src/main/java/control/Controller.java
+++ src/main/java/control/Controller.java
@@ -1,5 +1,31 @@
 package control;
 
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import ch.lepeit.brain2.model.Version;
+
+import ch.lepeit.brain2.BrainService;
+
+@Named
+//scope.... 
 public class Controller {
 
+    @Inject
+    private Logger log;
+
+    @Inject
+    private BrainService brainService;
+    
+    public void machNeueVersion(){
+        
+        Version version = null;
+        //bla bla
+        brainService.addVersion(version); 
+        
+        
+    }
+    
 }
